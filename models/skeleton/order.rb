523875e920b01a60ef3d5911033fcf1e1876c58e
leandrosardi <leandro.sardi@expandedventure.com>
2022-11-29 18:12:22 -0300

    leandrosardi/leads#95

--- models/skeleton/order.rb
+++ models/skeleton/order.rb
@@ -2,7 +2,7 @@ module BlackStack
     module DfyLeads
         class Order < Sequel::Model(:scr_order)
             many_to_one :user, :class=>:'BlackStack::MySaaS::User', :key=>:id_user
-            many_to_one :search, :class=>:'Leads::FlSearch', :key=>:id_search
+            many_to_one :search, :class=>:'BlackStack::Leads::Search', :key=>:id_search
             one_to_many :pages, :class=>:'BlackStack::DfyLeads::Page', :key=>:id_order
             
             TYPE_SNS = 0 # Sales Navigator Search
