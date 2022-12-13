20977fd2fb316880a24c7a14b7157be6cacd22c6
leandrosardi <leandro.sardi@expandedventure.com>
2022-12-13 11:41:34 -0300

    #3

--- sql/20221129.1.3.sql
+++ sql/20221129.1.3.sql
@@ -1,81 +1,24 @@
--- scraping order is about replicating the fitlers of an `fl_search` on any site linke LinkedIn, and upload the HTML pages of its results.
--- user can request a reprocessing of the same search by duplicating the order.
-create table if not exists scr_order (
-    id uuid not null primary key,
-    create_time timestamp not null,
-    id_user uuid not null references "user"(id),
-    "type" int not null, -- 0: sales navigator (use a fl_search object to save its filters).
-    id_search uuid /*not*/ null references fl_search(id),
-    "url" text /*not*/ null -- the user don't have to provide it. It is added later by automation or manually.
-);
-
-alter table scr_order add column if not exists "name" varchar(500) not null;
-alter table scr_order add column if not exists delete_time timestamp null;
-alter table scr_order add column if not exists "status" boolean not null;
-
--- after processing, the same process add all the leads to an export list for automated CSV file generation.
-alter table scr_order add column if not exists id_export uuid /*not*/ null references fl_export("id");
-
--- HTML downloaded from the browser, and uploaded to CS, by the extension.
-create table if not exists scr_page (
-    id uuid not null primary key,
-    create_time timestamp not null,
-    id_order uuid not null references scr_order(id),
-    "number" bigint not null, -- page number
-    -- pampa fields for parsing
-    parse_reservation_id varchar(500) null,
-    parse_reservation_time timestamp null,
-    parse_reservation_times int null,
-    parse_start_time timestamp null,
-    parse_end_time timestamp null,
-    parse_success boolean null,
-    parse_error_description text null
-);
-
 -- Link page results with a lead. 
 -- Note that the same lead may be found by more than 1 search.
-create table if not exists scr_result (
+create table if not exists dfyl_result (
     id uuid not null primary key,
     create_time timestamp not null,
     id_page uuid not null references scr_page(id),
     id_lead uuid not null references fl_lead(id)
 );
 
--- Trace when a user has its extension ACTIVE.
-create table if not exists scr_activity (
-    id uuid not null primary key,
-    create_time timestamp not null,
-    id_user uuid not null references "user"(id),
-    "year" int not null,
-    "month" int not null,
-    "day" int not null,
-    "hour" int not null,
-    "minute" int not null,
-    active boolean not null
-);
+-- people results have pagination, so we need to know the page number.
+alter table scr_page add column if not exists "number" bigint not null default 0;
+ 
+-- use a fl_search object to save its filters.
+alter table scr_order add column if not exists id_search uuid /*not*/ null references fl_search(id);
 
--- adapt scr_order to manage scraping, and assign to a user.
-alter table scr_order add column if not exists assign_reservation_id varchar(500) null;
-alter table scr_order add column if not exists assign_reservation_time timestamp null;
-alter table scr_order add column if not exists assign_reservation_times int null;
-alter table scr_order add column if not exists assign_start_time timestamp null;
-alter table scr_order add column if not exists assign_end_time timestamp null;
-alter table scr_order add column if not exists assign_success boolean null;
-alter table scr_order add column if not exists assign_error_description text null;
+-- after processing, the same process add all the leads to an export list for automated CSV file generation.
+alter table scr_order add column if not exists id_export uuid /*not*/ null references fl_export("id");
 
 -- scr_order search to track stats about the scraping, appending, and verification
-alter table scr_order add column if not exists stat_sns_search_leads bigint null;
-alter table scr_order add column if not exists stat_sns_scraped_pages bigint null;
-alter table scr_order add column if not exists stat_sns_scraped_leads bigint null;
-alter table scr_order add column if not exists stat_sns_leads_appended bigint null;
-alter table scr_order add column if not exists stat_sns_leads_verified bigint null;
-
--- the leads module will allow to filter by dfy_leads orders.
---alter table fl_search add column if not exists id_order uuid references scr_order(id) null;
-
--- users can install the linkedin-scraper extension on their browsers, and use it to scrape leads.
-alter table "user" add column if not exists linkedin_scraper_last_ping_time timestamp null;
-alter table "user" add column if not exists linkedin_scraper_last_ping_version varchar(500) null;
-
--- users can share their browsers to scrape other people orders and earch money.
-alter table "user" add column if not exists linkedin_scraper_share boolean not null default false;
+alter table scr_order add column if not exists dfyl_stat_search_leads bigint null; -- how many leads were found in the source (eg: sales nav)
+alter table scr_order add column if not exists dfyl_stat_scraped_pages bigint null; 
+alter table scr_order add column if not exists dfyl_stat_scraped_leads bigint null;
+alter table scr_order add column if not exists dfyl_stat_leads_appended bigint null;
+alter table scr_order add column if not exists dfyl_stat_leads_verified bigint null;
